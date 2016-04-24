df1f7079d612a0a0005041c12b100edfc241f309
Jay Ravaliya <jayrav13@gmail.com>
2016-04-24 09:33:38 -0400

    Final commit

--- RISE-Demo-App/ViewController.swift
+++ RISE-Demo-App/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var myTextField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func button(sender: AnyObject) {
+        self.textLabel.text = self.textField.text
+    }
+    
+    
 }
 
